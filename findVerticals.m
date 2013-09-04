d19d8fe6bd8d5e3a103dc71e43facc512f0552d4
freefood89 <freefood89@gmail.com>
2013-09-04 00:54:33 -0400

    Added cell header

--- findVerticals.m
+++ findVerticals.m
@@ -8,7 +8,7 @@ addpath([path(1:end-length('findVeritcals')) 'img1']);
 img = imread('purnell1.jpg');
 img_bw = rgb2gray(img);
 figure; imshow(img_bw);
-%%
+%% 
 vert = zeros(95,8);
 for n = 0:5
     vert(16*n+1:16*(n+1),n+1)=-1;
@@ -18,10 +18,11 @@ vert=fliplr(vert);
 figure; imagesc(vert);
 %%
 edges=abs(conv2(img_bw,vert,'same'));
-figure; imagesc(abs(edges));
+% figure; imagesc(abs(edges));
 
-%%
+%% Highlight discovered edges
+
 figure; hist(edges(:),50);
 [y,x] = find(edges>2500);
 figure; imagesc(edges); hold on;
-plot(x,y,'ro'); hold off;
+plot(x,y,'r.'); hold off;
