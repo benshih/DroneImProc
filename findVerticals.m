9b85115ec6c83233318ffb6d699f9a939ea35836
freefood89 <freefood89@gmail.com>
2013-09-05 00:38:36 -0400

    found the location of the longest vertical edge

--- findVerticals.m
+++ findVerticals.m
@@ -6,8 +6,13 @@ path = mfilename('fullpath');
 addpath([path(1:end-length('findVeritcals')) 'img1']);
 %%
 img = imread('purnell1.jpg');
-img_bw = rgb2gray(img);
-figure; imshow(img_bw);
+img_gray = rgb2gray(img);
+img_blue = img(:,:,3);
+img_green= img(:,:,2);
+
+[imwidth, imheight] = size(img_gray);
+
+% figure; imshow(img_gray);
 %% 
 vert = zeros(95,8);
 for n = 0:5
@@ -16,13 +21,38 @@ for n = 0:5
 end
 vert=fliplr(vert);
 figure; imagesc(vert);
-%%
-edges=abs(conv2(img_bw,vert,'same'));
-% figure; imagesc(abs(edges));
+%% Find vertical edges in skyline
 
-%% Highlight discovered edges
-
-figure; hist(edges(:),50);
-[y,x] = find(edges>2500);
-figure; imagesc(edges); hold on;
+edges=abs(conv2(img_blue,vert,'same'));
+edges_rot = imrotate(edges,rad2deg(atan(13/287)));
+figure; imagesc(edges_rot);
+% figure; imagesc(edges);
+figure; hist(edges_rot(:),50);
+[y,x] = find(edges_rot>3000);
+figure; imagesc(img_blue); hold on;
 plot(x,y,'r.'); hold off;
+
+%% Identify contiguous of edges
+
+[xnum,xbins]=hist(x,1:imwidth); 
+[ynum,ybins]=hist(y,1:imheight); 
+
+figure;
+subplot(121);bar(xbins,xnum);title('X-coord for edges'); hold on;
+longEdge = max(xnum);
+plot(xbins(xnum==longEdge), longEdge,'ro'); hold off;
+subplot(122);bar(ybins,ynum);title('Y-coord for edges');
+
+[y2,x2] = find(edges_rot(:,xbins(xnum==longEdge))>3000);
+hold on; plot(y2,200*ones(1,length(y2)),'ro'); hold off;
+%%
+figure; imagesc(imrotate(img_blue,rad2deg(atan(13/287)))); hold on;
+plot(x,y,'r.');
+plot(xbins(xnum==longEdge),y2,'k.'); hold off;
+%%
+% edges=abs(conv2(img_green,vert,'same'));
+% figure; imagesc(edges);
+% figure; hist(edges(:),50);
+% [y,x] = find(edges>3000);
+% figure; imagesc(img_green); hold on;
+% plot(x,y,'r.'); hold off;
