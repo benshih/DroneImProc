c6ba62d6fb7b2724028a51efef56387abebabfeb
freefood89 <freefood89@gmail.com>
2013-09-15 01:05:35 -0400

    cropping can speed up line detection

--- findBuildings.m
+++ findBuildings.m
@@ -8,19 +8,31 @@ addpath([path(1:end-length('findVeritcals')) 'img1']);
 img = imread('purnell1.jpg');
 img = rgb2gray(img);
 
-se = strel('square', 4);
+se = strel('disk', 4);
 imD = imdilate(img, se);
 imE = imerode(img, se);
-imEdge = imD-imE;
+imEdge = double(imD-imE);
 figure(1); imagesc(imEdge); title('Result of Morphological Filter');
 
 %% Analyze edges
-figure; hist(double(imEdge(:)),100); 
+figure(2); hist(double(imEdge(:)),100); 
 title('Histogram of Filter Output Intensity');
 
-[y,x] = find(imEdge==max(imEdge(:)));
+maxval = max(imEdge(:));
+[y,x] = find(imEdge==maxval);
 figure(1);
 hold on; plot(x,y,'k.'); hold off;
 
 %% Traverse Along Minimum Gradient
+[imWidth, imHeight] = size(img);
+cropX = max(0,x(1)-50):min(x(1)+50,imWidth);
+cropY = max(0,y(1)-50):min(y(1)+50,imHeight);
 
+crop = imEdge(cropX,cropY);
+figure(3); imagesc(crop);
+figure(4); hist(crop(:),200);
+
+gradient = diff(crop);
+se = strel('disk', 4);
+% gradient = imerode(gradient,se);
+figure(5); imagesc(gradient);
