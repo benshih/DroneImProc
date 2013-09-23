dd58cc19da779c3ce35d5ed81560720d2255eee3
freefood89 <freefood89@gmail.com>
2013-09-23 14:31:41 -0400

    Removed video playing part

--- fgdetect.m
+++ fgdetect.m
@@ -35,18 +35,11 @@ C = squeeze(video(250,200,:,:));
 figure;
 subplot(2,2,1); imshow(video(:,:,:,1));
 hold on; plot([100 100 250],[100 200 200],'ro'); hold off;
-subplot(2,2,2); plot(1:nFrames,A(1)); title('Pixel at (100,100)'); 
+subplot(2,2,2); plot(1:nFrames,A); title('Pixel at (100,100)'); 
 xlabel('frame'); ylabel('intensity');
-subplot(2,2,3); plot(1:nFrames,B(1)); title('Pixel at (100,200)'); 
+subplot(2,2,3); plot(1:nFrames,B); title('Pixel at (100,200)'); 
 xlabel('frame'); ylabel('intensity');
-subplot(2,2,4); plot(1:nFrames,C(1)); title('Pixel at (250,200)'); 
+subplot(2,2,4); plot(1:nFrames,C); title('Pixel at (250,200)'); 
 xlabel('frame'); ylabel('intensity');
 set(findall(gcf,'type','text'),'fontSize',16,'fontWeight','bold')
 
-for k=2:nFrames
-    subplot(2,2,1); imshow(video(:,:,:,k));
-hold on; plot([100 100 250],[100 200 200],'ro'); hold off;
-subplot(2,2,2); plot(1:k,A(:,1:k));
-subplot(2,2,3); plot(1:k,B(:,1:k));
-subplot(2,2,4); plot(1:k,C(:,1:k));
-end
