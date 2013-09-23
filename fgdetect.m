6e73eeaeb36dcd7721e083571186f91f0e309e5d
freefood89 <freefood89@gmail.com>
2013-09-23 14:29:35 -0400

    Plot the pixel intensity of 3 point over time

---
+++ fgdetect.m
@@ -0,0 +1,52 @@
+%% Foreground Detection
+% This script will demonstrate foreground detection
+%% Initialization
+
+clear all
+close all
+
+%% Load Video
+
+filename = 'kittenslide_clip.mp4';
+obj = VideoReader(filename);
+nFrames = obj.NumberOfFrames;
+vidHeight = obj.Height;
+vidWidth = obj.Width;
+mov(1:nFrames) = struct('cdata', zeros(vidHeight, vidWidth, 3, 'uint8'), 'colormap', []);
+video = read(obj);
+
+% Read one frame at a time.
+for k = 1:nFrames
+    % take a portion of the video
+    %mov(k).cdata = video(ceil(vidHeight/4):ceil(3/4*vidHeight),ceil(vidWidth/4):ceil(3/4*vidWidth),:,k);
+    mov(k).cdata = video(:,:,:,k);
+
+    mov(k).colormap = [];
+end
+
+%% Inspecting individual pixels
+
+
+
+A = squeeze(video(100,100,:,:));
+B = squeeze(video(100,200,:,:));
+C = squeeze(video(250,200,:,:));
+
+figure;
+subplot(2,2,1); imshow(video(:,:,:,1));
+hold on; plot([100 100 250],[100 200 200],'ro'); hold off;
+subplot(2,2,2); plot(1:nFrames,A(1)); title('Pixel at (100,100)'); 
+xlabel('frame'); ylabel('intensity');
+subplot(2,2,3); plot(1:nFrames,B(1)); title('Pixel at (100,200)'); 
+xlabel('frame'); ylabel('intensity');
+subplot(2,2,4); plot(1:nFrames,C(1)); title('Pixel at (250,200)'); 
+xlabel('frame'); ylabel('intensity');
+set(findall(gcf,'type','text'),'fontSize',16,'fontWeight','bold')
+
+for k=2:nFrames
+    subplot(2,2,1); imshow(video(:,:,:,k));
+hold on; plot([100 100 250],[100 200 200],'ro'); hold off;
+subplot(2,2,2); plot(1:k,A(:,1:k));
+subplot(2,2,3); plot(1:k,B(:,1:k));
+subplot(2,2,4); plot(1:k,C(:,1:k));
+end
