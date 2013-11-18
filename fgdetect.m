16629775ddd4675ebd98792a165fe2c291a8fcbc
freefood89 <freefood89@gmail.com>
2013-11-18 12:03:16 -0500

    cleaned up code and fixed plotting. also changed all operations to uint8

--- fgdetect.m
+++ fgdetect.m
@@ -16,76 +16,38 @@ mov(1:nFrames) = struct('cdata', zeros(vidHeight, vidWidth, 3, 'uint8'), 'colorm
 video = read(obj);
 
 % Read one frame at a time.
-for k = 1:nFrames
-    % take a portion of the video
-    %mov(k).cdata = video(ceil(vidHeight/4):ceil(3/4*vidHeight),ceil(vidWidth/4):ceil(3/4*vidWidth),:,k);
-    mov(k).cdata = video(:,:,:,k);
-    
-    mov(k).colormap = [];
-end
+% for k = 1:nFrames
+%     % take a portion of the video
+%     %mov(k).cdata = video(ceil(vidHeight/4):ceil(3/4*vidHeight),ceil(vidWidth/4):ceil(3/4*vidWidth),:,k);
+%     mov(k).cdata = double(video(:,:,:,k));
+%     
+%     mov(k).colormap = [];
+% end
 
-%% Inspecting individual pixels
-
-% A = squeeze(video(100,100,:,:));
-% B = squeeze(video(100,200,:,:));
-% C = squeeze(video(250,200,:,:));
-%
-% figure;
-% subplot(2,2,1); imshow(video(:,:,:,1));
-% hold on; plot([100 100 250],[100 200 200],'ro'); hold off;
-% subplot(2,2,2); plot(1:nFrames,A); title('Pixel at (100,100)');
-% xlabel('frame'); ylabel('intensity');
-% subplot(2,2,3); plot(1:nFrames,B); title('Pixel at (100,200)');
-% xlabel('frame'); ylabel('intensity');
-% subplot(2,2,4); plot(1:nFrames,C); title('Pixel at (250,200)');
-% xlabel('frame'); ylabel('intensity');
-% set(findall(gcf,'type','text'),'fontSize',16,'fontWeight','bold')
-
-%% Average pixel value
-% mu = mean(video,4);
-% figure, imshow(mu/255);
 %% Moving Average
-dur = 128;
+dur = 32;
 try
     %     close figure 100
     close figure 101
 catch me
 end
 % figure(100); set(100,'Position',[0 0 vidWidth vidHeight]);
-figure(101); %set(100,'Position',[vidWidth+100 0 vidWidth vidHeight]);
+% figure(101); set(101,'Position',[vidWidth+100 0 vidWidth vidHeight]);
 mu=0;
 sum=0;
 gig=0;
-for k = 1:nFrames
+mu = uint8(squeeze(mean(video(:,:,:,1:dur-1),4)));
+for k = dur:200%nFrames
+    mu = mu-video(:,:,:,k-dur+1)/dur;
     mu = mu+video(:,:,:,k)/dur;
-    if(k>dur)
-        mu = mu-video(:,:,:,k-dur)/dur;
-        delta=0;
-        for n=k-dur:k
-            delta = video(:,:,:,n)-mu;
-            sum = sum + (delta).^2;
-        end
-        sig = sqrt(double(sum)/(dur-1));
-        mask = abs(delta)>sig;
-        mask = mask(:,:,1)&mask(:,:,2)&mask(:,:,3);
-        figure(101); imshow(double(mask));
+    for n=k-dur+1:k
+        delta = video(:,:,:,k)-mu;
+        sum = sum + (delta).^2;
     end
-    
-    
-    %     figure(100); imshow(mu);
-    
+    sig = sqrt(double(sum)/dur)*2;
+    mask = abs(delta)>sig;
+    mask = mask(:,:,1)&mask(:,:,2)&mask(:,:,3);
+%     output(:,:,k) = mask;
+    figure(101), imshow(mask);
+    k
 end
-
-%% Thresholding
-%
-% figure(3);
-% for k = 32:nFrames
-%     clip = video(:,:,:,k-31:k);
-%     mu = mean(clip,4);
-%     sigma = sqrt(var(double(clip),0,4));
-%     maskR = abs(double(video(:,:,1,k))-mu(:,:,1))>1*sigma(:,:,1);
-%     maskG = abs(double(video(:,:,2,k))-mu(:,:,2))>1*sigma(:,:,2);
-%     maskB = abs(double(video(:,:,3,k))-mu(:,:,3))>1*sigma(:,:,3);
-%     mask = maskR & maskB & maskG;
-%     figure(3); imshow(double(mask).*double(video(:,:,1,k)));
-% end
