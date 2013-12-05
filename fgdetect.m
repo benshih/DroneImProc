b3026b1426e1b900649e22de4a6881bbbde4324d
freefood89 <freefood89@gmail.com>
2013-12-05 08:48:19 -0500

    resized video to half size. outputs demo. move demo generator to other mfile

--- fgdetect.m
+++ fgdetect.m
@@ -12,9 +12,15 @@ obj = VideoReader(filename);
 nFrames = obj.NumberOfFrames;
 vidHeight = obj.Height;
 vidWidth = obj.Width;
-mov(1:nFrames) = struct('cdata', zeros(vidHeight, vidWidth, 3, 'uint8'), 'colormap', []);
-video = read(obj);
 
+% mov(1:nFrames) = struct('cdata', zeros(vidHeight, vidWidth, 3, 'uint8'), 'colormap', []);
+video = read(obj);
+video1 = zeros(240,320,3,nFrames);
+%%
+for n=1:nFrames
+   video1(:,:,:,n) = double(imresize(squeeze(video(:,:,:,n)),[240 320]));
+end
+%%
 % Read one frame at a time.
 % for k = 1:nFrames
 %     % take a portion of the video
@@ -25,7 +31,8 @@ video = read(obj);
 % end
 
 %% Moving Average
-dur = 32;
+dur = 128;
+start = 250;
 try
     %     close figure 100
     close figure 101
@@ -33,21 +40,30 @@ catch me
 end
 % figure(100); set(100,'Position',[0 0 vidWidth vidHeight]);
 % figure(101); set(101,'Position',[vidWidth+100 0 vidWidth vidHeight]);
-mu=0;
 sum=0;
-gig=0;
-mu = uint8(squeeze(mean(video(:,:,:,1:dur-1),4)));
-for k = dur:200%nFrames
-    mu = mu-video(:,:,:,k-dur+1)/dur;
-    mu = mu+video(:,:,:,k)/dur;
+sig=0;
+mu = squeeze(mean(video1(:,:,:,start-dur:start-1),4));
+for k = start:nFrames
+    mu = mu-(video1(:,:,:,k-dur)/dur);
+    mu = mu+(video1(:,:,:,k-1)/dur);
     for n=k-dur+1:k
-        delta = video(:,:,:,k)-mu;
-        sum = sum + (delta).^2;
+        delta = (video1(:,:,:,k))-mu;
     end
-    sig = sqrt(double(sum)/dur)*2;
-    mask = abs(delta)>sig;
+    sig = sqrt(var((video1(:,:,:,k-dur:k-1)),0,4));
+    mask = abs(delta)>2*sig;
     mask = mask(:,:,1)&mask(:,:,2)&mask(:,:,3);
-%     output(:,:,k) = mask;
-    figure(101), imshow(mask);
+    output(:,:,k) = mask;
+%     figure(101), imshow(mask);
     k
 end
+%%
+obj2 = VideoWriter('oneMode240x320.avi');
+open(obj2);
+frame = uint8(zeros(240,640,3));
+for n=start:nFrames
+    frame(:,1:320,:) = uint8(video1(:,:,:,n));
+    frame(:,321:end,:) = 255*uint8(repmat(output(:,:,n),[1 1 3]));
+    writeVideo(obj2,uint8(frame));
+    figure(1), imshow(frame);
+end
+close(obj2);
