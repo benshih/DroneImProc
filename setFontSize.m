131f8e5216290eeff9337940047e9a2f78183fc3
freefood89 <freefood89@gmail.com>
2014-01-17 06:27:35 -0500

    removed files that do not belong

--- setFontSize.m
+++
@@ -1,6 +0,0 @@
-function setFontSize(fsize)
-    set(gca,'fontsize',fsize);
-    set(get(gca,'xlabel'),'fontsize',fsize)
-    set(get(gca,'ylabel'),'fontsize',fsize)
-    set(get(gca,'title'),'fontsize',fsize)
-end
