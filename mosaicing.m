f16d58e046b1f7ad6f6a7f60da9ef97619fb1f5b
freefood89 <freefood89@gmail.com>
2014-01-17 06:24:17 -0500

    copy pasted working files over from branch stabilization

--- mosaicing.m
+++ mosaicing.m
@@ -1,25 +1,27 @@
 %% Mosaicing
 %% Pull in Video
-clear all
+% clear all
 close all
 
-fname = 'gymBasketballClip.mp4';
+fname = 'gymBasketballClip2.mp4';
 obj = VideoReader(fname);
 nFrames = obj.NumberOfFrames;
 vidHeight = obj.Height;
 vidWidth = obj.Width;
 
 %% Read Video Object
-mov(1).cdata = read(obj,1);;
-mov(1).colormap = [];
-bigImage = mov(1).cdata;
-for k=2:floor(nFrames/3);
-    vid = read(obj,k);
-    mov(k).cdata = vid;
-    mov(k).colormap = [];
-    [a,b]=cpselect(mov(k).cdata, bigImage,'Wait',true);
+K = [40 91 139:nFrames];
+bigImage = lensdistort(read(obj,1),-.19);
+for n=1:length(K)
+    k=K(n);
+    frame = lensdistort(read(obj,k),-.19);
+    [a,b]=cpselect( bigImage,frame,'Wait',true);
+    if(size(a,1)>0)
     % homography to update bigImage
-    
+        bigImage = uint8(panorama( double(bigImage), double(frame),[a b]'));
+        k
+    end
+%     figure; imshow(mov(k).cdata); hold on; plot(a(:,1),a(:,2),'o'); hold off;
 end
 
 %% Play Video
