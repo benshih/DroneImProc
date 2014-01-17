c5bbab21411af767f56dcf5513e4ee4a566a3600
freefood89 <freefood89@gmail.com>
2014-01-17 00:17:44 -0500

    minor changes

--- mosaicing.m
+++ mosaicing.m
@@ -3,7 +3,7 @@
 clear all
 close all
 
-fname = 'C:\tmp\2014-01-15_04-32-24\gymBasketball.m4v';
+fname = 'gymBasketballClip.mp4';
 obj = VideoReader(fname);
 nFrames = obj.NumberOfFrames;
 vidHeight = obj.Height;
@@ -12,11 +12,14 @@ vidWidth = obj.Width;
 %% Read Video Object
 mov(1).cdata = read(obj,1);;
 mov(1).colormap = [];
+bigImage = mov(1).cdata;
 for k=2:floor(nFrames/3);
     vid = read(obj,k);
     mov(k).cdata = vid;
     mov(k).colormap = [];
-    [a,b]=cpselect(mov(k).cdata, mov(k-1).cdata,'Wait',true);
+    [a,b]=cpselect(mov(k).cdata, bigImage,'Wait',true);
+    % homography to update bigImage
+    
 end
 
 %% Play Video
