5ba05ac2bbb79a8a62af364489b271da035055a3
freefood89 <freefood89@gmail.com>
2014-02-23 22:02:32 -0500

    PlayVideo plays video

--- opencvtests/PlayVideo.cpp
+++ opencvtests/PlayVideo.cpp
@@ -18,22 +18,24 @@ int main(int argc, char *argv[])
 
     printf("1");
 
-    VideoCapture capture(argv[1]);
-
-
-    if(!capture.isOpened())
-    {
-        printf("Failed to open the video\n");
-        return -1;
-    }
+	VideoCapture cap;
+	if(argc > 1)
+		cap.open(string(argv[1]));
+	else
+		cap.open(0);
+	Mat fram; namedWindow("video",1);
 
     cout << "reached here";
 
+    Mat frame;
     for(;;)
     {
-        Mat frame;
-        capture >> frame; // get a new frame from camera
-        cout << "hi";
+        cap >> frame; // get a new frame from camera
+        if(!frame.data)
+		break;
+	imshow("video", frame);
+	if(waitKey(30) >=0)
+		break;
     }
 
     return 0;
