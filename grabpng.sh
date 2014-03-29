4026a3c200c1b5dd62623cfcb7be784d26bc6aa5
freefood89 <freefood89@gmail.com>
2014-03-29 19:45:21 -0400

    starts png server, download 1 png per second with numbered file name, echoes file name to standard out

---
+++ grabpng.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Initiating javascript png server"
+node node_modules/ar-drone/examples/png-stream.js &
+sleep 5
+echo "grabbing pngs"
+
+a=1
+while [ $a -lt 6 ]; do
+    curl -s "127.0.0.1:8080" -o "${a}.png"
+    echo "${a}.png"
+    let a=a+1
+    sleep 1
+done
