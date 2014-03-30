03a866950cb6d8ddf03f2aa3d219c0b9aeb47bd4
freefood89 <freefood89@gmail.com>
2014-03-30 01:38:25 -0400

    dumb changes

--- grabpng.sh
+++ grabpng.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "Initiating javascript png server"
-node node_modules/ar-drone/examples/png-stream.js &
+#node node_modules/ar-drone/examples/png-stream.js &
 sleep 5
 echo "grabbing pngs"
 
 a=1
-while [ $a -lt 6 ]; do
+while [ $a -lt 31 ]; do
     curl -s "127.0.0.1:8081" -o "${a}.png"
     echo "${a}.png"
+    echo -en "\007"
     let a=a+1
-    sleep 1
+    sleep 2
 done
