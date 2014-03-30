28a341107c4b59b026bb5f9b8b2d146a7dac4018
freefood89 <freefood89@gmail.com>
2014-03-30 00:59:14 -0400

    png-stream now handles SIGTERM by closing server

--- grabpng.sh
+++ grabpng.sh
@@ -7,7 +7,7 @@ echo "grabbing pngs"
 
 a=1
 while [ $a -lt 6 ]; do
-    curl -s "127.0.0.1:8080" -o "${a}.png"
+    curl -s "127.0.0.1:8081" -o "${a}.png"
     echo "${a}.png"
     let a=a+1
     sleep 1
