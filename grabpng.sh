ec9d35a40cb98175f98805c092fbd347107e210f
freefood89 <freefood89@gmail.com>
2014-04-01 19:38:00 -0400

    takes png server port as argument

--- grabpng.sh
+++ grabpng.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+then
+    PORT=8082
+    echo "Grabbing PNG from default port 8082"
+elif [ $# -eq 1 ]
+then
+    PORT=$1
+    echo "Grabbing PNG from port ${1}"
+fi
+
 echo "Initiating javascript png server"
-#node node_modules/ar-drone/examples/png-stream.js &
-sleep 5
+node simple_flight.js &
 echo "grabbing pngs"
 
+sleep 5
 a=1
-while [ $a -lt 31 ]; do
-    curl -s "127.0.0.1:8081" -o "${a}.png"
+while [ $a -lt 45 ]; do
+    curl "127.0.0.1:${PORT}" -o "${a}.png"
     echo "${a}.png"
-    echo -en "\007"
     let a=a+1
-    sleep 2
+    sleep 0.5
 done
