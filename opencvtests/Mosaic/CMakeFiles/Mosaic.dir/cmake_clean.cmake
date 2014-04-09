e9b4c69a74c4c581b4fb91d43f9b109056b43e12
Benjamin Shih <benjshih@gmail.com>
2014-04-09 00:14:32 -0400

    adding stitching code to a separate branch

---
+++ opencvtests/Mosaic/CMakeFiles/Mosaic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Mosaic.dir/Mosaic.cpp.o"
+  "Mosaic.pdb"
+  "Mosaic"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Mosaic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ opencvtests/Mosaic/CMakeFiles/Mosaicking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Mosaicking.dir/Mosaicking.cpp.o"
+  "Mosaicking.pdb"
+  "Mosaicking"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Mosaicking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
