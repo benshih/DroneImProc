775cec41879f059465bd9a58caf2191b090a08bb
freefood89 <freefood89@gmail.com>
2014-04-18 21:14:07 -0400

    removed cmake

--- opencvtests/PanoStitch/cmake_install.cmake
+++ opencvtests/PanoStitch/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/benshih/Desktop/BenProjectsActive/DroneImProc/opencvtests/PanoStitch
+# Install script for directory: /Users/rmatsuka/DroneImProc/opencvtests/PanoStitch
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -33,7 +33,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/Users/benshih/Desktop/BenProjectsActive/DroneImProc/opencvtests/PanoStitch/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/Users/rmatsuka/DroneImProc/opencvtests/PanoStitch/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/Users/benshih/Desktop/BenProjectsActive/DroneImProc/opencvtests/PanoStitch/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/Users/rmatsuka/DroneImProc/opencvtests/PanoStitch/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
